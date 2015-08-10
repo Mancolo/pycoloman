624f5e23da9c63d32e8ba423ab28aba81e0097dd
mancolo <Mancolo@users.noreply.github.com>
2015-08-09 22:28:28 -0700

    Added crm models to admin

--- pcm/crm/admin.py
+++ pcm/crm/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer, CustomerKind, Contact
+
+
+admin.site.register(Customer)
+admin.site.register(CustomerKind)
+admin.site.register(Contact)
